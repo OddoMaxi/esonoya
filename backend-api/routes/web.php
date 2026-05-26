4a965d233fe37562df8af85ac6ffab40b80be3fa
OddoMaxi <73769710+OddoMaxi@users.noreply.github.com>
2026-05-26 17:02:48 +0000

    temp: add /server-ip route to get outbound IP for SMS provider whitelist

--- backend-api/routes/web.php
+++ backend-api/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/server-ip', function () {
+    $ip = file_get_contents('https://api.ipify.org');
+    return response()->json(['outbound_ip' => $ip]);
+});
